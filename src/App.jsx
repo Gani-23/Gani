fdd5a177da4074b9a66753cc0a014421a4043fc0
gani-23 <saiganesh7989@gmail.com>
2024-06-04 16:12:38 +0530

    patch-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Intro from './Components/Intro';
 import StartIntro from './Components/Startintro';
+import Techstack from './Components/Techstack';
 
 function App() {
   return(
     <>
     <Intro />
     <StartIntro/>
+    <Techstack/>
     </>
   )
 
